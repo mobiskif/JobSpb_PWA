740e8e6525218a253ce0a3db2e4acab0683585c6
user <mobiskif@hotmail.com>
2020-04-26 15:33:37 +0300

    26/04/20

---
+++ js/restrict_map_area.js
@@ -0,0 +1,99 @@
+var myMap;
+var query;
+ymaps.ready(init);
+
+function init() {
+    myMap = new ymaps.Map('map', {
+        center: [59.938, 30.3],
+        zoom: 10,
+        //controls: ['smallMapDefaultSet']
+        controls: []
+    }, {
+        restrictMapArea: [
+            [59.738, 29.511],
+            [60.086, 30.829]
+        ]
+    });
+
+    MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+        '<div style="color: #000000; font-weight: bold;">$[properties.iconContent]</div>'
+    );
+
+    var searchControl = new ymaps.control.SearchControl({
+        options: {
+            noSuggestPanel: true
+        }
+    });
+
+    //var obj = document.querySelector('ymaps[class="ymaps-2-1-76-searchbox__button-cell"]');
+    var btn = document.querySelector('button[id="button"]');
+    btn.addEventListener('click', function (event) {
+        //var txt = document.querySelector('input[id="suggest"]');
+        var query = document.getElementById("suggest").value;
+        var salary = document.getElementById("salary").value;
+        //query = txt.getAttribute("text");
+        do_query(query, salary);
+    });
+
+    do_query("Токарь", "60000");
+}
+
+function addballons(data) {
+    $.each(data.items, function (key, val) {
+        if (val.address != null) {
+            console.log(val.address.lat + "," + val.address.lng);
+            var sal;
+            if (val.salary.from != null) sal = val.salary.from;
+            else if (val.salary.to != null) sal = val.salary.to;
+            myMap.geoObjects.add(new ymaps.Placemark([val.address.lat, val.address.lng], {
+                    hintContent: '<b>' + val.employer.name + '</b><br/>'
+                        + val.name + '<br/>'
+                        + val.salary.from + '..' + val.salary.to,
+                    iconContent: sal,
+                    balloonContent: '<b>' + val.employer.name + '</b><br/>'
+                        + '<a href="' + val.alternate_url + '" target="_blank">' + val.name + '</a><br/>'
+                        + val.salary.from + '..' + val.salary.to
+                        + '</br><a href="https://meet.google.com/hga-tzxn-fgq" target="_blank">HR отдел онлайн</a>'
+                },
+                {
+                    preset: 'islands#circleDotIcon',
+                    iconColor: 'green',
+                    // Опции.
+                    // Необходимо указать данный тип макета.
+                    iconLayout: 'default#imageWithContent',
+                    // Своё изображение иконки метки.
+                    //iconImageHref: 'images/ball.png',
+                    // Размеры метки.
+                    iconImageSize: [48, 48],
+                    // Смещение левого верхнего угла иконки относительно
+                    // её "ножки" (точки привязки).
+                    iconImageOffset: [-24, -24],
+                    // Смещение слоя с содержимым относительно слоя с картинкой.
+                    iconContentOffset: [8, 10],
+                    // Макет содержимого.
+                    iconContentLayout: MyIconContentLayout
+
+                }));
+        }
+    });
+}
+
+function addpage(i, q, s) {
+    console.log(i);
+    $.getJSON("https://api.hh.ru/vacancies?area=2&only_with_salary=true&salary=" + s + "&page=" + i + "&text=" + q, function (data) {
+        addballons(data);
+    });
+}
+
+function do_query(q, s) {
+    console.log(q);
+    myMap.geoObjects.removeAll();
+    $.getJSON("https://api.hh.ru/vacancies?area=2&only_with_salary=true&salary=" + s + "&page=0&text=" + q, function (data) {
+        var pags = data.pages;
+        console.log(pags);
+        addballons(data);
+        for (let i = 1; i < pags; i++) addpage(i, q, s);
+    });
+
+}
+
--- service-worker.js
+++ service-worker.js
@@ -1,55 +1,35 @@
-'use strict';
-
-// CODELAB: Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v4';
-
-// CODELAB: Add list of files to cache here.
-const FILES_TO_CACHE = [
-  '/offline.html',
+const cacheName = 'cache-v3';
+const precacheResources = [
+    '/',
+    'index.html',
+    'styles/main.css',
+    'images/space1.jpg',
+    'images/space2.jpg',
+    'images/space3.jpg'
 ];
 
-self.addEventListener('install', (evt) => {
-  console.log('[ServiceWorker] Install');
-  // CODELAB: Precache static resources here.
-evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('[ServiceWorker] Pre-caching offline page');
-      return cache.addAll(FILES_TO_CACHE);
-    })
-);
-  self.skipWaiting();
+self.addEventListener('install', event => {
+    console.log('Service worker install event!');
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(cache => {
+                return cache.addAll(precacheResources);
+            })
+    );
 });
 
-self.addEventListener('activate', (evt) => {
-  console.log('[ServiceWorker] Activate');
-  // CODELAB: Remove previous cached data from disk.
-evt.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(keyList.map((key) => {
-        if (key !== CACHE_NAME) {
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
-    })
-);
-  self.clients.claim();
+self.addEventListener('activate', event => {
+    console.log('Service worker activate event!');
 });
 
-self.addEventListener('fetch', (evt) => {
-  console.log('[ServiceWorker] Fetch', evt.request.url);
-  // CODELAB: Add fetch event handler here.
-if (evt.request.mode !== 'navigate') {
-  // Not a page navigation, bail.
-  return;
-}
-evt.respondWith(
-    fetch(evt.request)
-        .catch(() => {
-          return caches.open(CACHE_NAME)
-              .then((cache) => {
-                return cache.match('offline.html');
-              });
+self.addEventListener('fetch', event => {
+    console.log('Fetch intercepted for:', event.request.url);
+    event.respondWith(caches.match(event.request)
+        .then(cachedResponse => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+            return fetch(event.request);
         })
-);
+    );
 });
